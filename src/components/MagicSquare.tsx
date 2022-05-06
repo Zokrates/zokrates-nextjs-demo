f0beb06a2ee4276d9cc6f395d3299cfc3efb90cd
dark64 <darem966@gmail.com>
2022-05-07 01:40:23 +0200

    version 1

--- src/components/MagicSquare.tsx
+++ src/components/MagicSquare.tsx
@@ -1,22 +1,12 @@
-import { HStack, StackProps, VStack, Text } from "@chakra-ui/react";
-import { Input } from "@chakra-ui/react";
-import { useState } from "react";
-
-export type Value = {
-  value?: string;
-  solution: string;
-  static?: boolean;
-};
+import { HStack, Input, StackProps, VStack } from "@chakra-ui/react";
 
 export type MagicSquareProps = {
-  values: Value[][];
+  values: string[][];
   onValueChange: (row: number, cell: number, value: string) => void;
 };
 
 export const MagicSquare = (props: MagicSquareProps & StackProps) => {
   const { values, onValueChange, ...stackProps } = props;
-  const [showSolution, setShowSolution] = useState(false);
-
   return (
     <VStack w="200px" {...stackProps}>
       {values.map((row, r) => (
@@ -26,24 +16,16 @@ export const MagicSquare = (props: MagicSquareProps & StackProps) => {
               key={`${r}-${c}`}
               type="number"
               size="lg"
+              fontWeight="bold"
               textAlign="center"
-              value={showSolution ? value.solution : value.value ?? ""}
+              value={value ?? ""}
               onChange={(e) => onValueChange(r, c, e.currentTarget.value)}
-              color={showSolution ? "gray.500" : "current"}
-              readOnly={showSolution || value.static}
               max={100}
+              required={true}
             />
           ))}
         </HStack>
       ))}
-      <Text
-        color="gray.500"
-        onClick={() => setShowSolution(!showSolution)}
-        fontSize="sm"
-        cursor="pointer"
-      >
-        {showSolution ? "Hide" : "Show"} solution
-      </Text>
     </VStack>
   );
 };
--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,81 +1,153 @@
-import { Button, Code, Flex, Stack, Text } from "@chakra-ui/react";
-import { Link } from "@chakra-ui/react";
-import { useState } from "react";
+import {
+  Button,
+  Code,
+  Flex,
+  Link,
+  Stack,
+  Text,
+  useToast,
+} from "@chakra-ui/react";
+import { readFile } from "fs/promises";
+import { FormEvent, useEffect, useState } from "react";
+import { ZoKratesProvider } from "zokrates-js";
 import { Container } from "../components/Container";
 import { MagicSquare } from "../components/MagicSquare";
 
-const Index = () => {
+const initialize = async () => (await import("zokrates-js")).initialize();
+
+const solution = [
+  ["31", "73", "7"],
+  ["13", "37", "61"],
+  ["67", "1", "43"],
+];
+
+const Index = (props) => {
+  const [zokratesProvider, setZokratesProvider] =
+    useState<ZoKratesProvider>(null);
+  const [isLoading, setIsLoading] = useState(false);
+
+  useEffect(() => {
+    const load = async () => {
+      let provider = await initialize();
+      setZokratesProvider(provider);
+    };
+    load();
+  }, []);
+
   const [values, setValues] = useState([
-    // first row
-    [
-      { value: null, solution: "31" },
-      { value: null, solution: "73" },
-      { value: "7", solution: "7", static: true },
-    ],
-
-    // second row
-    [
-      { value: "13", solution: "13", static: true },
-      { value: "37", solution: "37", static: true },
-      { value: null, solution: "61" },
-    ],
-
-    // third row
-    [
-      { value: null, solution: "67" },
-      { value: null, solution: "1" },
-      { value: null, solution: "43" },
-    ],
+    ["", "", "7"],
+    ["13", "37", ""],
+    ["", "", ""],
   ]);
 
   const onValueChange = (r: number, c: number, value: string) => {
     let newValues = values.slice();
-    newValues[r][c] = {
-      ...values[r][c],
-      value,
-    };
-    console.log(newValues);
+    newValues[r][c] = value;
     setValues(newValues);
   };
 
+  const toast = useToast();
+
+  const onSubmit = (e: FormEvent<HTMLFormElement>) => {
+    e.preventDefault();
+    setIsLoading(true);
+
+    setTimeout(() => {
+      try {
+        const program = Uint8Array.from(Buffer.from(props.program, "hex"));
+        const inputs = values.flat();
+
+        const { witness } = zokratesProvider.computeWitness(program, [
+          ...inputs,
+          "111",
+        ]);
+
+        const provingKey = new Uint8Array(Buffer.from(props.provingKey, "hex"));
+        const proof = zokratesProvider.generateProof(
+          program,
+          witness,
+          provingKey
+        );
+
+        if (zokratesProvider.verify(props.verificationKey, proof)) {
+          toast({
+            title: "You got it!",
+            description: "Your solution has been successfully verified :)",
+            position: "top",
+            status: "success",
+            duration: 5000,
+            isClosable: true,
+          });
+        } else {
+          throw new Error("Verification failed");
+        }
+      } catch (e) {
+        console.error(e);
+        toast({
+          title: "Whoops!",
+          description: "Your solution seems to be incorrect :(",
+          position: "top",
+          status: "error",
+          duration: 5000,
+          isClosable: true,
+        });
+      }
+
+      setIsLoading(false);
+    }, 300);
+  };
+
   return (
-    <Container height="100vh">
-      <Stack
-        spacing="1.5rem"
-        width="100%"
-        maxWidth="48rem"
-        alignItems="center"
-        pt="8rem"
-        px="1rem"
-      >
-        <MagicSquare
-          marginBottom="2"
-          values={values}
-          onValueChange={(r, c, v) => onValueChange(r, c, v)}
-        />
-        <Text color="text" fontSize="lg">
-          This is a{" "}
-          <Text as="span" fontWeight="bold">
-            Magic Square
-          </Text>
-          . This means that the numbers add up to the same total in every
-          direction. Every row, column and diagonal should add up to{" "}
-          <Code>111</Code>. Fill the missing fields and verify the solution
-          without revealing it!
-        </Text>
-        <Button
-          // isLoading
-          // loadingText="Verifying"
-          colorScheme="teal"
-          variant="solid"
+    <Container height="100vh" justifyContent="center">
+      <form onSubmit={(e) => onSubmit(e)}>
+        <Stack
+          spacing="1.5rem"
+          width="100%"
+          maxWidth="48rem"
+          alignItems="center"
+          pt="8rem"
+          px="1rem"
         >
-          Verify solution
-        </Button>
-      </Stack>
+          <Flex direction="column" alignItems={"center"}>
+            <MagicSquare
+              values={values}
+              onValueChange={(r, c, v) => onValueChange(r, c, v)}
+              marginBottom="2"
+            />
+            <Text
+              color="gray.500"
+              fontSize="sm"
+              onClick={() => setValues(solution)}
+              cursor="pointer"
+            >
+              Show solution
+            </Text>
+          </Flex>
+          <Text color="text" fontSize="lg">
+            This is a{" "}
+            <Text as="span" fontWeight="bold">
+              Magic Square
+            </Text>
+            . This means that the numbers add up to the same total in every
+            direction. Every row, column and diagonal should add up to{" "}
+            <Code>111</Code>. Fill the missing fields and verify the solution
+            using zero knowledge proofs!
+          </Text>
+          <Button
+            isLoading={isLoading}
+            loadingText="Verifying..."
+            colorScheme="teal"
+            variant="solid"
+            type="submit"
+          >
+            Verify solution
+          </Button>
+        </Stack>
+      </form>
       <Flex as="footer" py="8rem">
-        <Text color="dimgray">
+        <Text color="gray.500">
           Made by{" "}
-          <Link color="text" href="https://github.com/Zokrates/ZoKrates">
+          <Link color="teal" href="https://github.com/Zokrates/ZoKrates">
             ZoKrates Team
           </Link>
         </Text>
@@ -83,5 +155,21 @@ const Index = () => {
     </Container>
   );
 };
+
+export async function getStaticProps() {
+  const program = (await readFile("zkp/magic_square")).toString("hex");
+
+  const verificationKey = JSON.parse(
+    (await readFile("zkp/verification.key")).toString()
+  );
+  const provingKey = (await readFile("zkp/proving.key")).toString("hex");
+  return {
+    props: {
+      program,
+      verificationKey,
+      provingKey,
+    },
+  };
+}
 
 export default Index;
