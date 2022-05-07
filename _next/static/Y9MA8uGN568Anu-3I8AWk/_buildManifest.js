a9eae36fcec86161fb3e99c1a32a5bdc9aa82b0d
dark64 <darem966@gmail.com>
2022-05-07 13:11:14 +0200

    Updates

---
+++ _next/static/Y9MA8uGN568Anu-3I8AWk/_buildManifest.js
@@ -0,0 +1,1 @@
+self.__BUILD_MANIFEST={__rewrites:{beforeFiles:[],afterFiles:[],fallback:[]},"/":["static/chunks/29107295-fbcfe2172188e46f.js","static/chunks/931-968418b91289228b.js","static/chunks/pages/index-b3ae159108347058.js"],"/_error":["static/chunks/pages/_error-0a004b8b8498208d.js"],sortedPages:["/","/_app","/_error"]},self.__BUILD_MANIFEST_CB&&self.__BUILD_MANIFEST_CB();
